d0e444c171dcbd1009dacfb4582add95276a1200
K413N <Kalen.Coolson@gmail.com>
2023-08-06 01:03:47 -0400

    removed div wrapper

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -10,12 +10,10 @@ console.info({ answer });
 
 function Game() {
   return (
-    <div>
   <form class="guess-input-wrapper">
     <label for="guess-input">Enter guess:</label>
     <input id="guess-input" type="text" />
   </form>
-  </div>
   )
 }
 
